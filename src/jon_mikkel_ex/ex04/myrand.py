46463037571b9dbe41e9df024e23ecadffb09542
Jon-Mikkel Korscik <jonkors@nmbu.no>
2019-10-22 02:10:57 +0200

    Can't test, testing in the testing project we created in class

--- src/jon_mikkel_ex/ex04/myrand.py
+++ src/jon_mikkel_ex/ex04/myrand.py
@@ -5,17 +5,16 @@ __email__ = "jonkors@nmbu.no"
 
 
 class LCGRand:
-    def _init__(self, seed):
-        self.seed = seed
+    def __init__(self, seed):
+        self.seed = [seed]
         self.a = 7**5
         self.m = 2**31-1
         self.n = 0
 
     def rand(self):
         self.n += 1
-        self.seed[self.n] = (self.a * self.seed[self.n - 1]) % self.m
+        self.seed.append((self.a * self.seed[self.n - 1]) % self.m)
         return self.seed
-
 
 
 
