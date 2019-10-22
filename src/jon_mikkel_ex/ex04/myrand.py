12e96f6c96c0ad7a438f9460a440f48375ba8a02
Jon-Mikkel Korscik <jonkors@nmbu.no>
2019-10-22 04:09:11 +0200

    Added some description to the myrand.py

--- src/jon_mikkel_ex/ex04/myrand.py
+++ src/jon_mikkel_ex/ex04/myrand.py
@@ -6,12 +6,24 @@ __email__ = "jonkors@nmbu.no"
 
 class LCGRand:
     def __init__(self, seed):
+        """
+
+        :param seed: A number selected when initiliazing, integer, set into
+        a list.
+        a: is a large odd number
+        m: is a large odd number
+        """
         self.seed = [seed]
         self.a = 7**5
         self.m = 2**31-1
         self.idx = 0
 
     def rand(self):
+        """
+        idx is updated each time rand is called. The seed list is appended
+        with the newly calculated "random number" based on the first seed.
+        :return: the last seed from the list. integer
+        """
         self.idx += 1
         self.seed.append((self.a * self.seed[self.idx - 1]) % self.m)
         return self.seed[self.idx]
