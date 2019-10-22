fd42a30880b5091e3937573689d6532102d37fde
Jon-Mikkel Korscik <jonkors@nmbu.no>
2019-10-22 02:21:33 +0200

    Made a draft for ListRand

--- src/jon_mikkel_ex/ex04/myrand.py
+++ src/jon_mikkel_ex/ex04/myrand.py
@@ -9,13 +9,28 @@ class LCGRand:
         self.seed = [seed]
         self.a = 7**5
         self.m = 2**31-1
-        self.n = 0
+        self.idx = 0
 
     def rand(self):
-        self.n += 1
-        self.seed.append((self.a * self.seed[self.n - 1]) % self.m)
-        return self.seed[self.n]
+        self.idx += 1
+        self.seed.append((self.a * self.seed[self.idx - 1]) % self.m)
+        return self.seed[self.idx]
 
 
+class ListRand:
+    def __init__(self, list_of_numbers):
+        self.list = list_of_numbers
+        self.a = 7 ** 5
+        self.m = 2 ** 31 - 1
+        self.idx = 0
 
-
+    def rand(self):
+        self.idx += 1
+        try:
+            self.list[self.idx] = (self.a * self.list[self.idx - 1]) % self.m
+            return self.list[self.idx]
+        except IndexError:
+            raise RuntimeError(
+                    'There are no more numbers to generate randomness from. \
+                    Try starting a new instance with a new list of numbers'
+                              )
