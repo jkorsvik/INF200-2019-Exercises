a898260e83196b493dd1d8faae84bcb09dacc6a6
Jon-Mikkel Korscik <jonkors@nmbu.no>
2019-10-22 04:03:00 +0200

    Walker wasn't quite finished, Can be improved in the initialising
    Im using a double for-loop :/

--- src/jon_mikkel_ex/ex04/walker.py
+++ src/jon_mikkel_ex/ex04/walker.py
@@ -4,16 +4,30 @@ __author__ = "Jon-Mikkel Korsvik"
 __email__ = "jonkors@nmbu.no"
 
 from random import getrandbits
+from pprint import pprint
 
 
 class Walker:
+    """
+    Walker class, with methods for checking position and moving.
+    Has its own method for finishing a trip home.
+
+    Input
+    ------
+    The club(starting position) - integer
+    Home(ending position) - integer
+    """
     def __init__(self, the_club, home):
-        self.position = 0
+        self.position = the_club
         self.home = home
-        self.start = the_club
         self.steps = 0
 
     def move(self):
+        """
+        Uses a really efficient cointoss-  bool(getrandbits(1)) gives true or
+        false. Registers each move as a step.
+        :return: New position after move
+        """
         if bool(getrandbits(1)):
             self.position += 1
         else:
@@ -29,3 +43,30 @@ class Walker:
 
     def get_steps(self):
         return self.steps
+
+    def make_the_trip(self):
+        while True:
+            self.move()
+            if self.is_at_home():
+                return self.get_steps()
+
+
+if __name__ == "__main__":
+    """
+    Making a list of the distances and making nested for-loop(woopsie)
+    for creating a dictionary over the number of steps for 5 experiments for 
+    each distance.
+    
+    Prints out a list of the number of steps per distance
+    """
+    list_of_distances = [1, 2, 5, 10, 20, 50, 100]
+    dict_of_path_lengths = dict()
+    for idx, end in enumerate(list_of_distances):
+        path_length = []
+        for _ in range(5):  # 5 Experiments per distance
+            walkiehomie = Walker(0, end)
+            path_length.append(walkiehomie.make_the_trip())
+        dict_of_path_lengths[end] = path_length
+        pprint(
+            f'Distance: {end} -> Path lengths : {dict_of_path_lengths[end]}'
+            )
